007526391515d3e6d769bc69757eae700d333ddc
chriswest1231@hotmail.com <>
2022-12-29 17:50:06 -0600

    Added player movement witht he old input system

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10.0f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput);
+
+        transform.position += movement * (movementSpeed * Time.deltaTime);
+    }
+}
