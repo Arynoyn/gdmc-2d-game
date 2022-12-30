f8efefbbd8cf8d05827d7c7656c3bd9e0036c6bb
chriswest1231@hotmail.com <>
2022-12-29 18:54:16 -0600

    Added Jump

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    [SerializeField]
-    private float movementSpeed = 10.0f;
+    [SerializeField] private float movementSpeed = 10.0f;
+    [SerializeField] private float jumpForce = 400;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(horizontalInput, verticalInput);
+        
+        Vector3 movement = new Vector3(horizontalInput, 0);
 
         transform.position += movement * (movementSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
