912be9094471da503affcd95955629ee5e29b9f4
chriswest1231@hotmail.com <>
2023-01-09 20:20:56 -0600

    Added Coin Ding Audio

---
+++ Assets/Scripts/CoinAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CoinAudio : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake() { _audioSource = GetComponent<AudioSource>(); }
+
+    void Start()
+    {
+        GameManager.Instance.OnCoinsChanged += _ => _audioSource.Play();
+    }
+}
