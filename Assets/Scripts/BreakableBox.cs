e09a353a3c7bb5d20f237036913f6cc368270a09
chriswest1231@hotmail.com <>
2023-01-13 14:13:48 -0600

    Added background music, audio Mixer, breakable box, and moving saw blade

---
+++ Assets/Scripts/BreakableBox.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BreakableBox : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        bool wasHitFromBelow = col.contacts[0].normal.y > 0;
+        bool isPlayer = col.collider.GetComponent<PlayerMovementController>() != null;
+
+        if (isPlayer
+            && wasHitFromBelow)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    [SerializeField] private Transform Start;
+    [SerializeField] private Transform End;
+    [SerializeField] private Transform Sprite;
+    
+    private float _positionPercentage = 0;
+    private int _direction = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _positionPercentage += Time.deltaTime * _direction;
+        Sprite.position = Vector3.Lerp(Start.position, End.position, _positionPercentage);
+
+        if (_positionPercentage >= 1 && _direction == 1)
+        {
+            _direction = -1;
+        }
+        else if (_positionPercentage <= 0 && _direction == -1)
+        {
+            _direction = 1;
+        }
+    }
+
+    
+}
