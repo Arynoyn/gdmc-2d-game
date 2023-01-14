738cb2f3a2139c70b19001a4b456476e954a8490
chriswest1231@hotmail.com <>
2023-01-13 20:02:16 -0600

    Add spin animation and speed adjustment to saw

--- Assets/Scripts/Saw.cs
+++ Assets/Scripts/Saw.cs
@@ -9,6 +9,8 @@ public class Saw : MonoBehaviour
     [SerializeField] private Transform Start;
     [SerializeField] private Transform End;
     [SerializeField] private Transform Sprite;
+    [SerializeField, Range(0f,2f)] private float Speed = 1.0f;
+    
     
     private float _positionPercentage = 0;
     private int _direction = 1;
@@ -16,7 +18,7 @@ public class Saw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _positionPercentage += Time.deltaTime * _direction;
+        _positionPercentage += Time.deltaTime * _direction * Speed;
         Sprite.position = Vector3.Lerp(Start.position, End.position, _positionPercentage);
 
         if (_positionPercentage >= 1 && _direction == 1)
