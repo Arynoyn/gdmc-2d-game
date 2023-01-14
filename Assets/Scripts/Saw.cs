e20ae0c803a23dfe956f64c4f63fa24fd296cfdd
chriswest1231@hotmail.com <>
2023-01-13 20:38:11 -0600

    Adjusted Saw speed to keep speed stable regardless of distance. Created Prefabs for Saw, Coinbox, and BreakableBox.

--- Assets/Scripts/Saw.cs
+++ Assets/Scripts/Saw.cs
@@ -9,7 +9,7 @@ public class Saw : MonoBehaviour
     [SerializeField] private Transform Start;
     [SerializeField] private Transform End;
     [SerializeField] private Transform Sprite;
-    [SerializeField, Range(0f,2f)] private float Speed = 1.0f;
+    [SerializeField, Range(0f,10f)] private float Speed = 1.0f;
     
     
     private float _positionPercentage = 0;
@@ -18,7 +18,10 @@ public class Saw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _positionPercentage += Time.deltaTime * _direction * Speed;
+        float distance = Vector3.Distance(Start.position, End.position);
+        float speedForDistance = Speed / distance;
+        
+        _positionPercentage += Time.deltaTime * _direction * speedForDistance;
         Sprite.position = Vector3.Lerp(Start.position, End.position, _positionPercentage);
 
         if (_positionPercentage >= 1 && _direction == 1)
