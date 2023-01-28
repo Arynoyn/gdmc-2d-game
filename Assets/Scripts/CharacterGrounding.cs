9d6c7aca0cea9f537cbf6b72c467562118f86a76
chriswest1231@hotmail.com <>
2023-01-28 14:22:10 -0600

    Added functionality for player to stick to moving objects

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -6,21 +6,52 @@ public class CharacterGrounding : MonoBehaviour
     [SerializeField] private Transform rightFoot;
     [SerializeField] private float maxDistance;
     [SerializeField] private LayerMask layerMask;
+    
     public bool IsGrounded { get; private set; }
 
-
+    private Transform groundedObject;
+    private Vector3? groundedObjectLastPosition;
+    
     // Update is called once per frame
     private void Update()
     {
         Vector2 leftFootPosition = leftFoot.position;
         Vector2 rightFootPosition = rightFoot.position;
         IsGrounded = CheckGrounding(leftFootPosition) || CheckGrounding(rightFootPosition);
+        
+        StickToGroundedObject();
     }
 
-    private bool CheckGrounding(Vector2 leftFootPosition)
+    private void StickToGroundedObject()
     {
-        RaycastHit2D raycastHit = Physics2D.Raycast(leftFootPosition, Vector2.down, maxDistance, layerMask);
-        Debug.DrawRay(leftFootPosition, Vector2.down * maxDistance, Color.red);
-        return raycastHit.collider != null;
+        if (groundedObject != null)
+        {
+            if (groundedObjectLastPosition.HasValue 
+                && groundedObjectLastPosition.Value != groundedObject.position)
+            {
+                Vector3 delta = groundedObject.position - groundedObjectLastPosition.Value;
+                transform.position += delta;
+            }
+
+            groundedObjectLastPosition = groundedObject.position;
+        }
+        else
+        {
+            groundedObjectLastPosition = null;
+        }
+    }
+
+    private bool CheckGrounding(Vector2 footPosition)
+    {
+        RaycastHit2D raycastHit = Physics2D.Raycast(footPosition, Vector2.down, maxDistance, layerMask);
+        Debug.DrawRay(footPosition, Vector2.down * maxDistance, Color.red);
+
+        var groundedObjectExists = raycastHit.collider != null;
+        
+        groundedObject = groundedObjectExists 
+            ? raycastHit.collider.transform 
+            : null;
+
+        return groundedObjectExists;
     }
 }
