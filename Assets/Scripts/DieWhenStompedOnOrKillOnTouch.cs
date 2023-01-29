2fb6ea4e7a35b866d9c50e715ec6b5178e464316
chriswest1231@hotmail.com <>
2023-01-29 16:50:52 -0600

    Spawn shell on death

--- Assets/Scripts/DieWhenStompedOnOrKillOnTouch.cs
+++ Assets/Scripts/DieWhenStompedOnOrKillOnTouch.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 public class DieWhenStompedOnOrKillOnTouch : MonoBehaviour
 {
+    [SerializeField] private GameObject prefabToSpawnOnStomp;
+    
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.WasHitFromTopSide() 
             && col.WasPlayer())
         {
+            if (prefabToSpawnOnStomp != null)
+            {
+                Instantiate(prefabToSpawnOnStomp, transform.position, transform.rotation);
+            }
             Destroy(gameObject);
         } else if (col.WasPlayer())
         {
