1f95fc6ae1dea8c37b2f1bd1fe564066ac562351
chriswest1231@hotmail.com <>
2023-01-29 13:31:09 -0600

    Added Snail

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Walker : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+    
+    private Collider2D _collider2D;
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _direction = Vector2.left;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider2D = GetComponent<Collider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + _direction * (_speed * Time.fixedDeltaTime));
+    }
+}
