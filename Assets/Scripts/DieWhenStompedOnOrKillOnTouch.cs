f3967ba55c14299cf53e2cfac2e5627dc64c8765
chriswest1231@hotmail.com <>
2023-01-29 16:13:08 -0600

    Added ability to stomp on snail

---
+++ Assets/Scripts/DieWhenStompedOnOrKillOnTouch.cs
@@ -0,0 +1,17 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+public class DieWhenStompedOnOrKillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.WasHitFromTopSide() 
+            && col.WasPlayer())
+        {
+            Destroy(gameObject);
+        } else if (col.WasPlayer())
+        {
+            GameManager.Instance.KillPlayer();
+        }
+    }
+}
