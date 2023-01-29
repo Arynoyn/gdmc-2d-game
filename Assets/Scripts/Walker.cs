2d0bcd9ddb2ceaa4656c886970c3f26e82a9cd20
chriswest1231@hotmail.com <>
2023-01-29 15:32:04 -0600

    Added require component tag to snail to require SpriteRenderer, added logic to turn around if hitting a non player object

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Walker : MonoBehaviour
 {
     [SerializeField] private float _speed = 1f;
@@ -29,25 +30,57 @@ public class Walker : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (ReachedEdge())
+        if (ReachedEdge() || HitNonPlayer())
         {
             SwitchDirection();
         }
     }
 
+    private bool HitNonPlayer()
+    {
+        float x = GetForwardX();
+        float y = transform.position.y;
+        
+        Vector2 colliderEdge = new Vector2(x, y);
+        Debug.DrawRay(colliderEdge, _direction * _maxDistance, Color.red);
+        RaycastHit2D raycastHit = Physics2D.Raycast(colliderEdge, _direction, _maxDistance);
+
+        if (raycastHit.collider == null)
+        {
+            return false;
+        }
+
+        if (raycastHit.collider.isTrigger)
+        {
+            return false;
+        }
+
+        if (raycastHit.collider.GetComponent<PlayerMovementController>() != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     private bool ReachedEdge()
     {
-        Bounds bounds = _collider2D.bounds;
-        float x = _direction == Vector2.left
-            ? bounds.min.x
-            : bounds.max.x;
-        float y = bounds.min.y;
+        float x = GetForwardX();
+        float y = _collider2D.bounds.min.y;
         
         Vector2 colliderEdge = new Vector2(x, y);
         Debug.DrawRay(colliderEdge, Vector2.down * _maxDistance, Color.red);
         
         RaycastHit2D raycastHit = Physics2D.Raycast(colliderEdge, Vector2.down, _maxDistance);
         return raycastHit.collider == null;
+    }
+
+    private float GetForwardX()
+    {
+        float x = _direction == Vector2.left
+            ? _collider2D.bounds.min.x
+            : _collider2D.bounds.max.x;
+        return x;
     }
 
     private void SwitchDirection()
